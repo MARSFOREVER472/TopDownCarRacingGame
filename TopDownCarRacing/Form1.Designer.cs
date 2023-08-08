a78a5e215eccdccde0c70ec5c3206efabf6f1eb8
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-08-08 13:59:12 -0400

    Se cargaron e importaron imágenes a la interfaz.

--- TopDownCarRacing/Form1.Designer.cs
+++ TopDownCarRacing/Form1.Designer.cs
@@ -30,47 +30,139 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.pistaCircuito1 = new System.Windows.Forms.PictureBox();
+            this.pistaCircuito2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pistaCircuito1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pistaCircuito2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Black;
-            this.panel1.Location = new System.Drawing.Point(53, 10);
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pistaCircuito2);
+            this.panel1.Controls.Add(this.pistaCircuito1);
+            this.panel1.Location = new System.Drawing.Point(20, 10);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(380, 424);
+            this.panel1.Size = new System.Drawing.Size(450, 532);
             this.panel1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(190, 475);
+            this.button1.Location = new System.Drawing.Point(190, 580);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 50);
             this.button1.TabIndex = 1;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // lblScore
             // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(188, 442);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 27);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Score: 0";
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(185, 550);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(120, 27);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Score: 0";
             // 
             // label2
             // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(10, 540);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 650);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(475, 215);
+            this.label2.Size = new System.Drawing.Size(475, 103);
             this.label2.TabIndex = 3;
             this.label2.Text = "Use Left and Right Key to move the car.\r\n\r\nDon\'t hit any other cars in the game a" +
     "nd survive as long as you can.\r\n";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pistaCircuito1
+            // 
+            this.pistaCircuito1.Image = global::TopDownCarRacing.Properties.Resources.roadTrack;
+            this.pistaCircuito1.Location = new System.Drawing.Point(0, -425);
+            this.pistaCircuito1.Name = "pistaCircuito1";
+            this.pistaCircuito1.Size = new System.Drawing.Size(450, 424);
+            this.pistaCircuito1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pistaCircuito1.TabIndex = 0;
+            this.pistaCircuito1.TabStop = false;
+            // 
+            // pistaCircuito2
+            // 
+            this.pistaCircuito2.Image = global::TopDownCarRacing.Properties.Resources.roadTrack;
+            this.pistaCircuito2.Location = new System.Drawing.Point(0, 0);
+            this.pistaCircuito2.Name = "pistaCircuito2";
+            this.pistaCircuito2.Size = new System.Drawing.Size(450, 532);
+            this.pistaCircuito2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pistaCircuito2.TabIndex = 1;
+            this.pistaCircuito2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TopDownCarRacing.Properties.Resources.carGreen;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 101);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TopDownCarRacing.Properties.Resources.carGrey;
+            this.pictureBox2.Location = new System.Drawing.Point(350, 25);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::TopDownCarRacing.Properties.Resources.bronze;
+            this.pictureBox3.Location = new System.Drawing.Point(50, 200);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(250, 100);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::TopDownCarRacing.Properties.Resources.explosion;
+            this.pictureBox4.Location = new System.Drawing.Point(75, 375);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::TopDownCarRacing.Properties.Resources.carYellow;
+            this.pictureBox5.Location = new System.Drawing.Point(205, 375);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(50, 99);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
             // 
             // Form1
             // 
@@ -78,12 +170,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(493, 764);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblScore);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "Car Racing Game";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pistaCircuito1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pistaCircuito2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -92,8 +193,15 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblScore;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pistaCircuito1;
+        private System.Windows.Forms.PictureBox pistaCircuito2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
 
